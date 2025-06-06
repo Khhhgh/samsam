05d03148c51b9fe77f5a9758b8ed0043f3ebde89
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2025-06-06 17:02:58 +0300

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -1,7 +1,7 @@
 # متطلبات التنصيب #
 OWNER = ["SN_R3F"]
 OWNER_NAME = "teto"
-BOT_TOKEN = "6432841340:AAFmzvjpBRc025leCsXzqafV7N1TLT_VLCc"
+BOT_TOKEN = "7672816252:AAF7WMsY2F4pBmodviKxyvdFHqg6kSJ-ftg"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/A_S_4A"
 GROUP = "https://t.me/mohhhgcc"
