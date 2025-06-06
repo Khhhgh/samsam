b9744eb9d978cff5f4f17771d9ff9ea99d72f44b
Khhhgh <103472630+Khhhgh@users.noreply.github.com>
2025-06-06 16:57:14 +0300

    Add files via upload

---
+++ OWNER.py
@@ -0,0 +1,10 @@
+# متطلبات التنصيب #
+OWNER = ["SN_R3F"]
+OWNER_NAME = "teto"
+BOT_TOKEN = "6432841340:AAFmzvjpBRc025leCsXzqafV7N1TLT_VLCc"
+DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
+CHANNEL = "https://t.me/A_S_4A"
+GROUP = "https://t.me/mohhhgcc"
+VIDEO = "https://graph.org/file/061951daec85b0f94b83c.jpg"
+LOGS = "zombie2"
+
